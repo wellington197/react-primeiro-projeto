ed33bfa70628e26e6824018ce81010019b4b2359
wellington197 <fcowellcosta@gmail.com>
2022-02-07 16:37:49 -0300

    update repository

--- react-vite/components/Photo.tsx
+++ react-vite/components/Photo.tsx
@@ -1,12 +1,15 @@
+type Props = {
+    url: string;
+    legend:string;
+}
 
 /*Criando componente Photo */
-export const Photo = () =>{
+export const Photo = ({url,legend}: Props) => {
     return (
         <div>
-            <img src=""/>
-            <p>...</p>
+            <img src={url}/>
+            <p>{legend}</p>
 
         </div>
-    )
-
+    );
 }
--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,10 +1,13 @@
-
-import Photo
+import {Photo} from '../components/Photo'
 
 const App = () => {
   return (
     <div>
       Hello Word!!!
+
+      <Photo url="http://www.google.com.br/google.jpg" legend="google"/>
+
+     
     </div>
     ); 
 }
